2b62dc5c357b37f9eeb07a36c778c14f5cd93852
dick en henriette <dickkyhenriette@outlook.com>
2016-10-08 13:59:15 +0200

    virtual protect

--- padScanFinal/sigscan.cs
+++ padScanFinal/sigscan.cs
@@ -1,31 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Runtime.InteropServices;
 
-namespace padScanFinal
+public class sigscan
 {
-    class sigscan
+    // REQUIRED CONSTS
+
+    const int PROCESS_QUERY_INFORMATION = 0x0400;
+    const int MEM_COMMIT = 0x00001000;
+    const int PAGE_READWRITE = 0x04;
+    const int PROCESS_WM_READ = 0x0010;
+
+    // REQUIRED METHODS
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+    [DllImport("kernel32.dll")]
+    public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+    [DllImport("kernel32.dll")]
+    public static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern int VirtualQueryEx(IntPtr hProcess,IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+    // REQUIRED STRUCTS
+    public struct MEMORY_BASIC_INFORMATION
     {
+        public int BaseAddress;
+        public int AllocationBase;
+        public int AllocationProtect;
+        public int RegionSize;
+        public int State;
+        public int Protect;
+        public int lType;
+    }
 
-        /// <summary> usage
-        /// string hoek = "\x48\x83\xec\x00\xe8";
-        ///byte[] getup = sigscan.ToByteArray(hoek);
-        ///string getstring = BitConverter.ToString(getup, 0);
-        ///label1.Text = getstring;
-        /// </summary>
-        public static byte[] ToByteArray(string value)
-           {
-            char[] charArr = value.ToCharArray();
-            byte[] bytes = new byte[charArr.Length];
-            for (int i = 0; i < charArr.Length; i++)
-            {
-                byte current = Convert.ToByte(charArr[i]);
-                bytes[i] = current;
-            }
+    public struct SYSTEM_INFO
+    {
+        public ushort processorArchitecture;
+        ushort reserved;
+        public uint pageSize;
+        public IntPtr minimumApplicationAddress;
+        public IntPtr maximumApplicationAddress;
+        public IntPtr activeProcessorMask;
+        public uint numberOfProcessors;
+        public uint processorType;
+        public uint allocationGranularity;
+        public ushort processorLevel;
+        public ushort processorRevision;
+    }
 
-            return bytes;
-          }
+    public static byte[] ToByteArray(string value)
+    {
+        char[] charArr = value.ToCharArray();
+        byte[] bytes = new byte[charArr.Length];
+        for (int i = 0; i < charArr.Length; i++)
+        {
+            byte current = Convert.ToByte(charArr[i]);
+            bytes[i] = current;
+        }
 
+        return bytes;
     }
 }
